ba7c11f2d274f0b9dc1f3b17ce6e8d65c48100d1
Ma5Codes <117619833+Ma5Codes@users.noreply.github.com>
2025-02-24 16:03:34 +0300

    commit!!

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,58 +1,94 @@
-import { Link, useSearchParams } from "react-router-dom"
-import { Button } from "./ui/button"
-import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react"
-import { Briefcase, BriefcaseBusiness, Heart, PenBox } from "lucide-react"
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignIn,
+  useUser,
+} from "@clerk/clerk-react";
+import { Button } from "./ui/button";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 
 const Header = () => {
   const [showSignIn, setShowSignIn] = useState(false);
+
   const [search, setSearch] = useSearchParams();
-  useEffect(() =>{
-    if (search.get("sign-in")){
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (search.get("sign-in")) {
       setShowSignIn(true);
     }
   }, [search]);
-  const handleOverlayClick = (e) => {if (e.target === e.currentTarget) {setShowSignIn(false); setSearch({});
-  }
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setShowSignIn(false);
+      setSearch({});
+    }
   };
+
   return (
     <>
-    <nav className="py-4 flex justify-between items-center">
-     <Link >
-      <img src="/logo.png" alt="logo" className="h-20" />
-     </Link>
-     <div className="flex gap-8">
-     <SignedOut>
-     <Button variant="outline" onClick={() => setShowSignIn(true)}>Login</Button>
-     </SignedOut>
-     <SignedIn>   
-      <Button variant="destructive" className='rounded-full'>
-        <PenBox size={20} className="mr-2"/>
-        Post a Job
+      <nav className="py-4 flex justify-between items-center">
+        <Link to="/">
+          <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
+        </Link>
 
-      </Button>
-      <Link to={"/post-job"}></Link>
-      <UserButton appearance={{elements:{avatarBox:"h-10 w-10",},}}>
-        <UserButton.MenuItems>
-          <UserButton.Link label="My Jobs" labelIcon={<BriefcaseBusiness size={15} />} href="/my-jobs" />
-          <UserButton.Link label="Saved Jobs" labelIcon={<Heart size={15} />} href="/saved-jobs" />
-        </UserButton.MenuItems>
-      </UserButton>
-      </SignedIn>
-      </div>
+        <div className="flex gap-8">
+          <SignedOut>
+            <Button variant="outline" onClick={() => setShowSignIn(true)}>
+              Login
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            {user?.unsafeMetadata?.role === "recruiter" && (
+              <Link to="/post-job">
+                <Button variant="destructive" className="rounded-full">
+                  <PenBox size={20} className="mr-2" />
+                  Post a Job
+                </Button>
+              </Link>
+            )}
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            >
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15} />}
+                  href="/my-jobs"
+                />
+                <UserButton.Link
+                  label="Saved Jobs"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-jobs"
+                />
+                <UserButton.Action label="manageAccount" />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
+      </nav>
 
-    </nav>
-    {showSignIn && (
-      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"onClick={handleOverlayClick}>
-        <SignIn
-          signUpForceRedirectUrl="/onboarding"
-          fallbackRedirectUrl="/onboarding"
+      {showSignIn && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+          <SignIn
+            signUpForceRedirectUrl="/onboarding"
+            fallbackRedirectUrl="/onboarding"
           />
-      </div>
-    )}
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
